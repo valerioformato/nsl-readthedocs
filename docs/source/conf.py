c5f782a06d5273bfaa023664d594547ad8e8d9b9
Valerio Formato <valerio.formato@cern.ch>
2023-01-27 12:44:13 +0100

    Change layout

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,3 +33,8 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
